6401c83e61c7401ac1245f1078b99fbf676135a1
Milad <miladmaps@gmail.com>
2021-11-30 00:23:30 +0330

    mock data

--- mock.js
+++ mock.js
@@ -1,7 +1,8 @@
 export const posts = [
   {
     _id: 1,
-    title: "Lorem, ipsum dolor.",
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
     views: "100",
     image: "/images/posts/office-computer-keyboard.jpg",
     body: "",
@@ -15,7 +16,38 @@ export const posts = [
   },
   {
     _id: 2,
-    title: "Lorem, ipsum dolor.",
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
+    views: "100",
+    image: "/images/posts/blog-2.jpg",
+    body: "",
+    publishedAt: "",
+    readingTime: "",
+    slug: "",
+    category: "Styling",
+    minutesToRead: 12,
+    summary:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
+  },
+  {
+    _id: 3,
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
+    views: "100",
+    image: "/images/posts/blog-1.jpeg",
+    body: "",
+    publishedAt: "",
+    readingTime: "",
+    slug: "",
+    category: "Styling",
+    minutesToRead: 12,
+    summary:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
+  },
+  {
+    _id: 4,
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
     views: "100",
     image: "/images/posts/digital-tablet.jpg",
     body: "",
@@ -28,10 +60,11 @@ export const posts = [
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
   },
   {
-    _id: 2,
-    title: "Lorem, ipsum dolor.",
+    _id: 5,
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
     views: "100",
-    image: "/images/posts/digital-tablet.jpg",
+    image: "/images/posts/blog-3.jfif",
     body: "",
     publishedAt: "",
     readingTime: "",
@@ -42,52 +75,11 @@ export const posts = [
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
   },
   {
-    _id: 2,
-    title: "Lorem, ipsum dolor.",
+    _id: 6,
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam",
     views: "100",
-    image: "/images/posts/digital-tablet.jpg",
-    body: "",
-    publishedAt: "",
-    readingTime: "",
-    slug: "",
-    category: "Styling",
-    minutesToRead: 12,
-    summary:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
-  },
-  {
-    _id: 2,
-    title: "Lorem, ipsum dolor.",
-    views: "100",
-    image: "/images/posts/digital-tablet.jpg",
-    body: "",
-    publishedAt: "",
-    readingTime: "",
-    slug: "",
-    category: "Styling",
-    minutesToRead: 12,
-    summary:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
-  },
-  {
-    _id: 2,
-    title: "Lorem, ipsum dolor.",
-    views: "100",
-    image: "/images/posts/digital-tablet.jpg",
-    body: "",
-    publishedAt: "",
-    readingTime: "",
-    slug: "",
-    category: "Styling",
-    minutesToRead: 12,
-    summary:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos officiis totam dicta molestiae quibusdam omnis eaque doloribus eos incidunt deserunt!",
-  },
-  {
-    _id: 2,
-    title: "Lorem, ipsum dolor.",
-    views: "100",
-    image: "/images/posts/digital-tablet.jpg",
+    image: "/images/posts/blog-4.jpeg",
     body: "",
     publishedAt: "",
     readingTime: "",
