d7ca1e3fad45da0096213d2690cbc5fad4b0b159
Milad <miladmaps@gmail.com>
2021-11-30 00:24:07 +0330

    next.config

--- next.config.js
+++ next.config.js
@@ -7,14 +7,15 @@ module.exports = {
     REACT_APP_BASE_URL: "lapertwear.com",
     REACT_APP_API_BASE_URL: "/api/proxy",
     REACT_APP_EXTERNAL_API_BASE_URL: "http://tpi.uneed.ir:7900",
+    // REACT_APP_FONT_ICON_API_TOKEN: "8716B435-A59C-44AB-9167-70E02B137A37",
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   i18n: {
-    locales: ["fa"],
-    // locales: ["fa", "en"],
-    defaultLocale: "fa",
+    locales: ["en"],
+    // locales: ["en", "fa"],
+    defaultLocale: "en",
   },
   experimental: {
     craCompat: true,
