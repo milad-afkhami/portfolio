a6a644e8dadd020bb487fb2a4ae3f144337d6e9d
Milad <dev.miladmaps@gmail.com>
2021-12-08 14:32:32 +0330

    featured posts

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { useHome } from "@hooks";
 import { Div } from "@kits";
-// import { ExploreView } from "@components/Explore";
 import { Head } from "@components/SEO";
 import {
-  ResumeContact,
   ResumeProfile,
+  FeaturedPosts,
   ResumeProjects,
+  ResumeContact,
 } from "@components/Resume";
 import { posts } from "mock";
 import dynamic from "next/dynamic";
-
-const FeaturedPosts = dynamic(() =>
-  import("@components/Resume").then((mod) => mod.FeaturedPosts)
-);
 
 const Home = (props) => {
   const { data, isValidating, mutate, error } = useHome();
@@ -23,7 +19,7 @@ const Home = (props) => {
       <Head canonical="/" />
       <Div width="100%" py="3">
         <ResumeProfile />
-        <FeaturedPosts posts={posts} />
+        <FeaturedPosts />
         <ResumeProjects />
         <ResumeContact />
       </Div>
