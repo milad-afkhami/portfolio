c87f445b384bf8c4047fe65afb103013776405a9
Milad <dev.miladmaps@gmail.com>
2021-12-08 14:31:05 +0330

    featured posts

--- components/Resume/FeaturedPosts/index.js
+++ components/Resume/FeaturedPosts/index.js
@@ -1,48 +1,18 @@
 import React from "react";
 import { Div, Link, Text } from "@kits";
-import { FeaturedPostCard } from "./Card";
+import { Blogs, FeaturedPostCard } from "@components/Blog";
+import { blogs } from "mock";
 
 export const FeaturedPosts = (props) => {
-  const { posts = [] } = props || {};
+  const {} = props || {};
 
   return (
-    <Div my="6">
-      <Div mb="4">
-        <Text size="h-md-b">resume.posts.title</Text>
-      </Div>
-      <Div
-        grid={["repeat(3, 1fr)", , "1rem", "1rem"]}
-        css={{ gridAutoFlow: "row" }}
-        overflowY="hidden"
-        overflowX="auto"
-        // Mh="calc(400px + 2rem)"
-        pb="3"
-        after={{
-          content: `\"\"`,
-          bg: `linear-gradient(-90deg, #333, transparent)`,
-          width: "3rem",
-          height: "100%",
-          position: "absolute",
-          right: 0,
-        }}
-      >
-        {posts.map((post, i) => (
-          <FeaturedPostCard key={i} {...post} />
-        ))}
-      </Div>
-      <Div>
-        <Link
-          href="/blog"
-          width="100%"
-          height="3rem"
-          flex={["center", "center"]}
-          bg="bg-secondary"
-          hoverBg="bg-secondary-hover"
-          curve="sm"
-        >
-          <Text>resume.posts.seeMore</Text>
-        </Link>
-      </Div>
+    <Div my="5">
+      <Blogs
+        items={blogs.slice(0, 6)}
+        title="resume.posts.title"
+        moreLink="/blog"
+      />
     </Div>
   );
 };
