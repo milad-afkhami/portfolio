95c88cbe02feb3168354591b5f4f1c9022805769
Milad <dev.miladmaps@gmail.com>
2021-12-12 21:43:24 +0330

    featured posts

--- components/Resume/FeaturedPosts/index.js
+++ components/Resume/FeaturedPosts/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Div, Link, Text } from "@kits";
 import { Blogs, FeaturedPostCard } from "@components/Blog";
 import { blogs } from "mock";
+import { useBlogs } from "@hooks";
 
 export const FeaturedPosts = (props) => {
   const {} = props || {};
 
+  const { data, isValidating, mutate, error } = useBlogs();
+
   return (
     <Div my="5">
       <Blogs
-        items={blogs.slice(0, 6)}
+        items={(data?.blogs).slice(0, 6)}
         title="resume.posts.title"
         moreLink="/blog"
       />
