4bb2a1ffeb21a7b88692b44d292b0aa25b51032d
Milad <dev.miladmaps@gmail.com>
2021-12-13 20:46:39 +0330

    blog-3

--- pages/api/blog/list.js
+++ pages/api/blog/list.js
@@ -9,23 +9,41 @@ export default async function blogsList(req, res) {
 
   const blogsPath2 = path.join(__dirname, "data");
   const blogsPath3 = path.join(__dirname, "..");
+  const blogsPath4 = path.join(__dirname, "..", "..");
+  const blogsPath5 = path.join(__dirname, "..", "..", "..");
 
   console.log({ blogsPath2, blogsPath3 });
 
-  const blogsFilePaths = fs
-    .readdirSync(blogsPath)
-    .filter((path) => /\.mdx?$/.test(path));
-
-  const blogs = blogsFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(blogsPath, filePath));
-    const { data, ...rest } = matter(source);
-
-    return {
-      ...data,
-      slug: filePath?.replace?.(/\.mdx?$/, ""),
-    };
+  fs.readdir(blogsPath2, (...result) => {
+    console.log("blogsPath2", { result });
   });
 
-  if (res) res.status(200).json({ blogs });
-  else return { blogs };
+  fs.readdir(blogsPath3, (...result) => {
+    console.log("blogsPath3", { result });
+  });
+
+  fs.readdir(blogsPath4, (...result) => {
+    console.log("blogsPath4", { result });
+  });
+
+  fs.readdir(blogsPath5, (...result) => {
+    console.log("blogsPath5", { result });
+  });
+
+  // const blogsFilePaths = fs
+  //   .readdirSync(blogsPath)
+  //   .filter((path) => /\.mdx?$/.test(path));
+
+  // const blogs = blogsFilePaths.map((filePath) => {
+  //   const source = fs.readFileSync(path.join(blogsPath, filePath));
+  //   const { data, ...rest } = matter(source);
+
+  //   return {
+  //     ...data,
+  //     slug: filePath?.replace?.(/\.mdx?$/, ""),
+  //   };
+  // });
+
+  if (res) res.status(200).json({ blogs: [] });
+  else return { blogs: [] };
 }
