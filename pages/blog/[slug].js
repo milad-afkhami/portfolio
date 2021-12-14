47e197ec4cbd17d63378765e7313c0685a6285ac
Milad <dev.miladmaps@gmail.com>
2021-12-14 20:25:36 +0330

    blog

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Breadcrumb, Div, Text } from "@kits";
 import { useRouter } from "@hooks";
 import { Head } from "@components/SEO";
 import { MDXRemote } from "next-mdx-remote";
 import dynamic from "next/dynamic";
 import { BlogMeta, BlogSummary, BlogTitle } from "@components/Blog";
-import { Http } from "@utils";
 
 const components = {
   Image: dynamic(() => import("@kits").then((module) => module.Image)),
@@ -19,11 +18,6 @@ export default function BlogPage(props) {
   const { title, summary, publishedAt, readingTime, category } =
     props?.blog?.frontMatter || {};
   const source = props?.blog?.source || {};
-
-  useEffect(() => {
-    Http.get({ url: "/api/blogget", proxyLayer: false });
-    Http.get({ url: "/blogget", proxyLayer: false });
-  }, []);
 
   return (
     <>
@@ -44,36 +38,36 @@ export default function BlogPage(props) {
             readingTime={readingTime}
             publishedAt={publishedAt}
           />
-          {/* <MDXRemote {...source} components={components} /> */}
+          <MDXRemote {...source} components={components} />
         </Div>
       </Div>
     </>
   );
 }
 
-// export const getStaticProps = async ({ params }) => {
-//   const getBlogDetail = require("@api/blogget").default;
+export const getStaticProps = async ({ params }) => {
+  const getBlogDetail = require("@api/blog/get").default;
 
-//   const result = await getBlogDetail(params);
+  const result = await getBlogDetail(params);
 
-//   return {
-//     props: {
-//       blog: result.blog,
-//     },
-//   };
-// };
+  return {
+    props: {
+      blog: result.blog,
+    },
+  };
+};
 
-// export const getStaticPaths = async () => {
-//   // const blogsList = require("@api/bloglist").default;
+export const getStaticPaths = async () => {
+  const blogsList = require("@api/blog/list").default;
 
-//   // #todo change data/blog directory folder structure to [blog-slug]/[locale].mdx in order to translate blogs.
+  // #todo change data/blog directory folder structure to [blog-slug]/[locale].mdx in order to translate blogs.
 
-//   // const result = await blogsList();
+  const result = await blogsList();
 
-//   // const paths = result.blogs.map(({ slug }) => ({ params: { slug } }));
+  const paths = result.blogs.map(({ slug }) => ({ params: { slug } }));
 
-//   return {
-//     paths: [{ params: { slug: "hello-world" } }],
-//     fallback: false,
-//   };
-// };
+  return {
+    paths,
+    fallback: false,
+  };
+};
