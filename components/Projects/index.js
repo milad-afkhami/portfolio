9799aac355eb3e82202620d783f435a6a8915c94
Milad <dev.miladmaps@gmail.com>
2021-12-16 19:11:39 +0330

    projects

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Div, Text } from "@kits";
-import { useState } from "@hooks";
+import { useState, useRouter } from "@hooks";
 import { projectsData } from "mock";
 import { Project } from "./Project";
 
 export const Projects = (props) => {
   const {} = props || {};
   const [state, setState] = useState(null);
+  const router = useRouter();
+
+  const projects =
+    projectsData[router.locale] || projectsData[router.locales[0]];
 
   return (
     <Div>
-      {projectsData.map((project, i) => (
+      {projects.map((project, i) => (
         <Project {...project} key={i} index={i + 1} />
       ))}
     </Div>
