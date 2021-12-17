8a5ba55288f4b382a3f4cd7df7eba6865911f95c
Milad <miladmaps@gmail.com>
2021-12-18 00:03:50 +0330

    contact options

--- components/Resume/Contact/Option.js
+++ components/Resume/Contact/Option.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Div, Icon, Image, Text } from "@kits";
+import { Div, Image, Text } from "@kits";
+import { toast } from "@utils";
+import { useTranslation } from "@hooks";
+import { THEMES } from "@constants";
 
 export function ResumeContactOption(props) {
-  const { type, value } = props || {};
+  const { type, label, value } = props || {};
+
+  const t = useTranslation().t;
+
+  const onClickContactWay = () => {
+    toast("layout.message.textCopied", { type: "info" });
+  };
 
   return (
-    <Div flex={["center", , "column"]}>
+    <Div
+      flex={["center", , "column"]}
+      Mw="100%"
+      overflow="hidden"
+      onClick={onClickContactWay}
+      cursor="pointer"
+    >
       <Div
         bg="brand"
         hoverBg="brand-hover"
@@ -14,31 +29,35 @@ export function ResumeContactOption(props) {
         height="100px"
         flex={["center", "center"]}
         position="relative"
-        cursor="pointer"
         mb="3"
         // responsive={{ md: { width: "100px", height: "100px" } }}
       >
         <Image
-          {...(type === "telegram"
-            ? {
-                src: `/images/icons/telegram.png`,
-                width: "100px",
-                height: "100px",
-                fit: "contain",
-              }
-            : {
-                src: `/images/icons/${type}.svg`,
-                layout: "fill",
-              })}
+          src={`/images/icons/contact/${type}.png`}
+          css={{
+            [`[data-theme="${THEMES.LIGHT}"] &`]: {
+              filter: "invert(1)",
+            },
+          }}
+          width="50px"
+          height="50px"
+          fit="contain"
         />
       </Div>
       <Div mb="2">
-        <Text color="brand" size="lg" css={{ textTransform: "uppercase" }}>
+        <Text color="brand" size="lg-b" css={{ textTransform: "capitalize" }}>
           {`resume.contact.${type}`}
         </Text>
       </Div>
-      <Div>
-        <Text color="brand-light">{value}</Text>
+      <Div
+        flex={["center", "center"]}
+        Mw="80%"
+        hover={{ css: { textDecoration: "underline" } }}
+        color="brand-light"
+      >
+        <Text color="brand-light" size="sm" Mw="100%" truncate>
+          {label || value}
+        </Text>
       </Div>
     </Div>
   );
