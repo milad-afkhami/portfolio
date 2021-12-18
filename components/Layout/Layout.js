14a9bd210aafce239a5ca8e1363cf285f11cc020
Milad <dev.miladmaps@gmail.com>
2021-12-18 10:51:28 +0330

    github corner

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,6 +3,12 @@ import { Div } from "@kits";
 import { APP_WRAPPER_ELEMENT_ID } from "@config";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import dynamic from "next/dynamic";
+
+const GithubCorner = dynamic(
+  () => import("./GithubCorner").then((module) => module.GithubCorner),
+  { ssr: false }
+);
 
 export const Layout = (props = {}) => {
   const { children, hideHeader, hideFooter, transparentHeader } = props;
@@ -15,6 +21,7 @@ export const Layout = (props = {}) => {
       mh="100vh"
       container
     >
+      <GithubCorner />
       <Div id="more-menu__list" />
       <Div id="modal__wrapper" />
       {!hideHeader && <Header transparent={transparentHeader} />}
