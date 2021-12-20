63c2ca7fb9e4b12b190cb93a8036cfdc9a312407
Milad <dev.miladmaps@gmail.com>
2021-12-20 20:43:37 +0330

    gists

---
+++ components/Gists/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Div, Text } from "@kits";
+import { useState } from "@hooks";
+
+export const Gists = (props) => {
+  const {} = props || {};
+  const [state, setState] = useState(null);
+
+  return <Div>Gists</Div>;
+};
