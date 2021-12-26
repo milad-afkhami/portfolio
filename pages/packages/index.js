640b78f22453babab6c7f345ea30070bb1e33d14
Milad <dev.miladmaps@gmail.com>
2021-12-26 19:17:41 +0330

    package

---
+++ pages/packages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Div } from "@kits";
+import { Head } from "@components/SEO";
+import { PageTitle } from "@components/Layout";
+
+export default function PackagesPage(props) {
+  return (
+    <>
+      <Head canonical="/packages" />
+      <Div width="100%" py="3">
+        <PageTitle title="packages.title" />
+      </Div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  return { props: { packages: [] } };
+}
