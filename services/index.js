2ca3b0ebcd6ebaeef82ce6502ec024a2d7143aed
Milad <dev.miladmaps@gmail.com>
2021-12-27 10:34:27 +0330

    services - clean up

--- services/index.js
+++ services/index.js
@@ -1,4 +1,2 @@
-export * from "./auth";
-export * from "./media";
 export * from "./blog";
 export * from "./gist";
--- services/media/index.js
+++
@@ -1,5 +0,0 @@
-import { uploadMedia } from "./upload";
-
-export class MediaServices {
-  static upload = uploadMedia;
-}
