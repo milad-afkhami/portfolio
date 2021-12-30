6ead897ed1508dc9ab9954e6079d5247240c2972
Milad <dev.miladmaps@gmail.com>
2021-12-30 09:26:15 +0330

    next.config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const domain = process.env.REACT_APP_BASE_URL;
 
 module.exports = {
   env: {
-    REACT_APP_BASE_URL: "lapertwear.com",
+    REACT_APP_BASE_URL: "portfolio-mili.vercel.app",
     REACT_APP_API_BASE_URL: "/api/proxy",
     REACT_APP_EXTERNAL_API_BASE_URL: "/api",
     // REACT_APP_FONT_ICON_API_TOKEN: "8716B435-A59C-44AB-9167-70E02B137A37",
