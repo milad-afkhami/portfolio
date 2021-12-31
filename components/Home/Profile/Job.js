8b331209a3fc379d418e8383a7d52d9a2e22b19b
Milad <miladmaps@gmail.com>
2021-12-31 16:52:32 +0330

    profile

--- components/Home/Profile/Job.js
+++ components/Home/Profile/Job.js
@@ -4,21 +4,31 @@ import { Div, Text } from "@kits";
 export function ProfileJob(props) {
   return (
     <Div>
-      <Div mb="3">
-        <Text size="xlg-b" tag="h2">
+      <Div
+        mb="3"
+        css={{ textAlign: "center" }}
+        responsive={{ md: { css: { textAlign: "start".imp() } } }}
+      >
+        <Text size="lg-b" tag="h2">
           home.profile.job
         </Text>
       </Div>
       <Div
         css={{ textAlign: "center" }}
-        responsive={{ md: { css: { textAlign: "start".imp() } } }}
+        responsive={{
+          md: {
+            css: {
+              textAlign: "start".imp(),
+              "*": { whiteSpace: "pre-line" },
+            },
+          },
+        }}
       >
         <Text
           size="lg"
           color="text-secondary"
           tag="h3"
           lineHeight="2"
-          css={{ whiteSpace: "pre-line" }}
           Mw="600px"
         >
           home.profile.jobDescription
