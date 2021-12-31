e809f0d512850f4470d6767ab22797437b9ff427
Milad <miladmaps@gmail.com>
2021-12-31 17:47:57 +0330

    about

--- components/About/Timeline/index.js
+++ components/About/Timeline/index.js
@@ -2,24 +2,9 @@ import React from "react";
 import { Div } from "@kits";
 import { AboutTimelineSection } from "./Section";
 import { SectionTitle } from "@components/Layout";
+import { aboutTimelineSections as sections } from "@config";
 
 export const AboutTimeLine = (props) => {
-  const sections = [
-    { year: 2022, items: ["ielts"] },
-    { year: 2021, items: ["startingAtArshCo", "becomingTeamLead"] },
-    {
-      year: 2020,
-      items: ["growingMyHair", "leftMyFirstJob", "startingAtAppTech"],
-    },
-    { year: 2019, items: ["changingField", "firstDayAtWork"] },
-    { year: 2018, items: ["university", "tookCoupleOfSemestersOff"] },
-    { year: 2014, items: ["choosingNEAPath"] },
-    { year: 2008, items: ["firstComputer"] },
-    { year: 2004, items: ["startingSchool"] },
-    { year: 2003, items: ["earlySchool"] },
-    { year: 1998, items: ["born"] },
-  ];
-
   return (
     <Div my="5">
       <SectionTitle icon="timeline" title="about.timeline.title" />
--- config.js
+++ config.js
@@ -18,6 +18,22 @@ const navLinks = [
   { link: "/gists", label: "gists" },
 ];
 
+const aboutTimelineSections = [
+  { year: 2022, items: ["ielts"] },
+  { year: 2021, items: ["startingAtArshCo", "becomingTeamLead"] },
+  {
+    year: 2020,
+    items: ["growingMyHair", "leftMyFirstJob", "startingAtAppTech"],
+  },
+  { year: 2019, items: ["changingField", "firstDayAtWork"] },
+  { year: 2018, items: ["university", "tookCoupleOfSemestersOff"] },
+  { year: 2014, items: ["choosingNEAPath"] },
+  { year: 2008, items: ["firstComputer"] },
+  { year: 2004, items: ["startingSchool"] },
+  { year: 2003, items: ["earlySchool"] },
+  { year: 1998, items: ["born"] },
+];
+
 export {
   appBaseURL,
   externalApiBaseURL,
@@ -27,4 +43,5 @@ export {
   proxyBasePath,
   proxyBasePathRegExp,
   navLinks,
+  aboutTimelineSections,
 };
