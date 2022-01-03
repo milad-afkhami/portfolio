eace74ee29081735ba7ee108c472f5a8b9daf33f
Milad <dev.miladmaps@gmail.com>
2022-01-03 16:30:48 +0330

    typography

--- stylesheets/utils/setTextTypography.js
+++ stylesheets/utils/setTextTypography.js
@@ -2,7 +2,12 @@ export const setTextTypography = (size = "md", options) => {
   const importance = options?.important ? " !important" : "";
   const bold = options?.bold;
 
-  const fontSize = `var(--font-size-${size}, ${size})`;
+  // existence of a "." in variable name will mark value as "invalid" and second parameter of var function(fallback value) won't work
+  // so for values like "2.5rem" or ..., we have to remove it in first param
+  // it will not have any effect on standard typography keys(xs, sm, ...)
+  // p.s. String.prototype.replaceAll didn't work properly on ssr so I used replace method with regExp instead
+
+  const fontSize = `var(--font-size-${size?.replace(/\./g, "")}, ${size})`;
 
   const fontWeight = bold ? "bold" : `var(--font-weight-${size}, ${size})`;
 
