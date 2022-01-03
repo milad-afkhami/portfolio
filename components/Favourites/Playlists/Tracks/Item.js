8bec3446e32623d7eacb1cf2d5829dadf7c43dc6
Milad <dev.miladmaps@gmail.com>
2022-01-03 16:28:54 +0330

    favourites

---
+++ components/Favourites/Playlists/Tracks/Item.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Div, Text } from "@kits";
+
+export const FavouritePlaylistTracksItem = (props) => {
+  const { index, name, artist } = props || {};
+
+  return (
+    <Div
+      flex={["center", "start"]}
+      height="62px"
+      // bg="bg-secondary"
+      hoverBg="bg-secondary"
+      responsive={{ sm: { px: "3" } }}
+      curve="sm"
+      my="2"
+      cursor="pointer"
+    >
+      <Div flex={["center", "center"]}>
+        <Text size="md" noTranslation>{`${index}. `}</Text>
+      </Div>
+      <Div mx="3" Mw="-webkit-fill-available">
+        <Div>
+          <Text size="xsm" color="text-secondary" noTranslation>
+            {artist}
+          </Text>
+        </Div>
+        <Div flex>
+          <Text size="sm" noTranslation truncate Mw="100%">
+            {name}
+          </Text>
+        </Div>
+      </Div>
+    </Div>
+  );
+};
