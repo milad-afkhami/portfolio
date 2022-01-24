18edba9aedd958931e9f48a05000e9eb72b8149e
Milad <dev.miladmaps@gmail.com>
2022-01-24 09:25:25 +0330

    typography

--- stylesheets/utils/setTextTypography.js
+++ stylesheets/utils/setTextTypography.js
@@ -1,4 +1,4 @@
-export const setTextTypography = (size = "md", options) => {
+export const setTextTypography = (size, options) => {
   const importance = options?.important ? " !important" : "";
   const bold = options?.bold;
 
@@ -7,12 +7,11 @@ export const setTextTypography = (size = "md", options) => {
   // it will not have any effect on standard typography keys(xs, sm, ...)
   // p.s. String.prototype.replaceAll didn't work properly on ssr so I used replace method with regExp instead
 
-  const fontSize = `var(--font-size-${size?.replace(/\./g, "")}, ${size})`;
+  const fontSize = `var(--font-size-${size?.replace?.(/\./g, "")}, ${size})`;
 
   const fontWeight = bold ? "bold" : `var(--font-weight-${size}, ${size})`;
 
-  return {
-    fontSize: fontSize + importance,
-    fontWeight: fontWeight + importance,
-  };
+  return size
+    ? { fontSize: fontSize + importance, fontWeight: fontWeight + importance }
+    : {};
 };
