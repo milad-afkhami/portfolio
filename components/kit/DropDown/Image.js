b40b74f8fc1f9f16138eaec680797a3f65b670e0
Milad <dev.miladmaps@gmail.com>
2022-01-26 18:20:22 +0330

    DropDown Kit

--- components/kit/DropDown/Image.js
+++ components/kit/DropDown/Image.js
@@ -6,7 +6,7 @@ export const DropDownImage = (props) => {
 
   return src ? (
     <Div flex={["center", "center"]} mx="2">
-      <Image src={src} width="20px" height="20px" />
+      <Image src={src} width="20px" height="20px" quality={1} />
     </Div>
   ) : null;
 };
