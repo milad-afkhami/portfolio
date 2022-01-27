b81d6d4c7fe9727be72fad6d36cd4d13e628b97c
Milad <dev.miladmaps@gmail.com>
2022-01-27 14:05:26 +0330

    divider kit - jsdoc

--- components/kit/Divider.js
+++ components/kit/Divider.js
@@ -1,17 +1,14 @@
 import { Div } from "@kits";
 
-type DividerProps = {
-  width?: String,
-  height?: String,
-  color?: String,
-  orientation?: "vertical" | "horizontal",
-};
-
+/**
+ * Renders a divider
+ * @param {{ width:string, height:string, color:string, orientation: ("vertical"|"horizontal") }} props
+ */
 export const Divider = ({
   color = "border-light",
   length = "100%",
   thickness = "1px",
   orientation = "horizontal",
-}): DividerProps => {
+}) => {
   return <Div width={width} height={height} bg={color} />;
 };
