bc67284c7bf2bda7b63d2e87f8cfbb0f4d94ac08
Milad <dev.miladmaps@gmail.com>
2022-01-29 13:43:40 +0330

    useToggle hook - jsdoc

--- hooks/useToggle.js
+++ hooks/useToggle.js
@@ -1,8 +1,11 @@
 import { useState, useCallback, useDebugValue } from "@hooks";
 
-type IToggleHook = [Boolean, () => void];
-
-export function useToggle(initialState: Boolean = false): IToggleHook {
+/**
+ * This hook Takes a parameter with value true or false and toggles that value to opposite. It's useful when we want to take some action into it's opposite action, for example: show and hide modal, show more/show less text, open/close side menu.
+ * @param {boolean} [initialState=false]
+ * @returns {[boolean, Function]}
+ */
+export function useToggle(initialState: Boolean = false) {
   const [state, setState] = useState(initialState);
 
   const toggle = useCallback(() => setState((state) => !state), []);
