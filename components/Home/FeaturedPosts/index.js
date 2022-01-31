7dfc6e62da90779d74f2950171037468b0ac0c2c
Milad <dev.miladmaps@gmail.com>
2022-01-31 14:10:56 +0330

    homepage - posts

--- components/Home/FeaturedPosts/index.js
+++ components/Home/FeaturedPosts/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import { Div } from "@kits";
 import { Blogs } from "@components/Blog";
 import { ORIENTATION } from "@constants";
+import { useMediaQuery } from "@hooks";
+import { breakpoints } from "@stylesheets";
 
 export const FeaturedPosts = (props) => {
   const { posts } = props || {};
-
   // const { data, isValidating, mutate, error } = useBlogs();
+
+  const isMediumScreen = useMediaQuery({
+    minWidth: breakpoints.sm,
+    maxWidth: breakpoints.md,
+  });
 
   return (
     <Div my="4">
       <Blogs
         orientation={ORIENTATION.HORIZONTAL}
-        items={posts?.slice?.(0, 3)}
+        items={posts?.slice?.(0, isMediumScreen ? 2 : 3)}
         title="home.posts.title"
         moreLink="/blog"
       />
