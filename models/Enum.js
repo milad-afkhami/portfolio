fa03b8a092f0e7b8a2f72e107fc437e411a1e536
Milad <dev.miladmaps@gmail.com>
2022-02-02 13:41:57 +0330

    Enum model

--- models/Enum.js
+++ models/Enum.js
@@ -1,26 +1,17 @@
-import { __keys } from "@utils";
-
 /**
- * creates an Enum ( not a complete instance of typescript enum )
+ * An Enum constructor in javascript, well not quite, not a complete instance of the Enum but a similar one.
  * @param args Enum members
- * @method next : this method gets a member of instanciated enum and returns the next member. if there wasn't any, it will return first parameter
+ * @method next this method gets a member of instanciated enum and returns the next member. if there wasn't any, it will return first parameter
  */
 export function Enum(...values) {
   for (const val of values) this[val] = val;
-
-  const next = (currentValue) => {
-    // to filter methods from members:
-    const members = __keys(this, (m) =>
-      ["string", "number"].includes(typeof m)
-    );
-    const currentIndex = members.indexOf(currentValue);
-
-    let nextIndex = (currentIndex + 1) % members.length;
-
-    return members[nextIndex];
-  };
-
-  return { ...Object.freeze(this), next };
 }
 
-// Enum.prototype.next = (currentValue) => {};
+Enum.prototype.next = function (currentValue) {
+  const members = Object.values(this);
+  const currentIndex = members.indexOf(currentValue);
+
+  let nextIndex = (currentIndex + 1) % members.length;
+
+  return members[nextIndex];
+};
