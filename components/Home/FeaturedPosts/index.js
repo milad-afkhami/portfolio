bdeeb62bd25bb6b0b69b70d6dfeddb9dadfdb54d
Milad <dev.miladmaps@gmail.com>
2022-02-02 19:10:45 +0330

    homepage - posts

--- components/Home/FeaturedPosts/index.js
+++ components/Home/FeaturedPosts/index.js
@@ -5,14 +5,28 @@ import { ORIENTATION } from "@constants";
 import { useMediaQuery } from "@hooks";
 import { breakpoints } from "@stylesheets";
 
+const homeBlogs = [
+  "data-fetching-pattern-preference",
+  "rethink-using-redux",
+  "homophobia",
+
+  // "redux-clean-code",
+  // "multi-theme",
+  // "typography",
+  // "http-proxy",
+];
+
 export const FeaturedPosts = (props) => {
-  const { posts } = props || {};
   // const { data, isValidating, mutate, error } = useBlogs();
 
   const isMediumScreen = useMediaQuery({
     minWidth: breakpoints.sm,
     maxWidth: breakpoints.md,
   });
+
+  const posts = homeBlogs.map((slug) =>
+    props.posts.find((it) => it.slug === slug)
+  );
 
   return (
     <Div my="4">
