674b1e2cf277f2a0971ef4ddbc7db8c531ccd603
Milad <dev.miladmaps@gmail.com>
2022-02-03 09:09:44 +0330

    projects

--- components/Projects/Techs/index.js
+++ components/Projects/Techs/index.js
@@ -6,7 +6,11 @@ export const ProjectTechs = (props) => {
   const { techs } = props || {};
 
   return (
-    <Div flex={["center", "start"]} css={{ gap: "var(--spacing-2)" }} mb="4">
+    <Div
+      flex={["center", "start", , "wrap"]}
+      css={{ gap: "var(--spacing-2)" }}
+      mb="4"
+    >
       {techs.map((tech, i) => (
         <ProjectTech {...tech} />
       ))}
