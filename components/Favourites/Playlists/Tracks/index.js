b4941288a1a805243aa5f94c4ef5cc6afd3672f1
Milad <dev.miladmaps@gmail.com>
2022-02-09 11:42:40 +0330

    favourites

--- components/Favourites/Playlists/Tracks/index.js
+++ components/Favourites/Playlists/Tracks/index.js
@@ -1,43 +1,22 @@
 import React from "react";
-import { Button, Div, Text } from "@kits";
-import { useMemo } from "@hooks";
-import { __chunk } from "@utils";
-import { FavouritePlaylistTracksItem } from "./Item";
+import { Div, ListView } from "@kits";
 
 export const FavouritePlaylistTracks = (props) => {
   const { open, tracks } = props || {};
 
-  const pivot = useMemo(() => Math.ceil(tracks.length / 2), [tracks.length]);
+  const row = Math.ceil(tracks.length / 2);
+  const height = open
+    ? `calc(${row} * var(--list-view-item-height) + 1rem)`
+    : "0px";
 
-  const columns = useMemo(() => __chunk(tracks, pivot), [tracks.length]);
-
-  // #todo refactor with ListView Kit
   return (
-    <Div
-      height={`calc(${open ? pivot : 0} * 70px)`}
-      pace="fast"
-      overflow="hidden"
-      my={open ? "3" : "0"}
-      css={{ sm: { mt: 0 } }}
-    >
-      <Div
-        height="100%"
-        width="100%"
-        border="border-light"
-        curve="xsm"
-        flex={[, , "row"]}
-      >
-        {columns.map((columnTracks, j) => (
-          <Div col={{ xs: 6, md: 6 }} my="-1" px="2">
-            {columnTracks.map((track, i) => (
-              <FavouritePlaylistTracksItem
-                index={j === 0 ? i + 1 : pivot + i + 1}
-                {...track}
-              />
-            ))}
-          </Div>
-        ))}
-      </Div>
+    <Div height={height} pace="normal" overflow="hidden">
+      <ListView
+        items={tracks.map((track) => ({
+          title: track.name,
+          subtitle: track.artist,
+        }))}
+      />
     </Div>
   );
 };
