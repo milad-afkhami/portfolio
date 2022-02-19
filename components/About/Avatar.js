88526c6c9ccd5bf54245eb870adeea28d07106be
Milad <miladmaps@gmail.com>
2022-02-19 21:56:21 +0330

    about

--- components/About/Avatar.js
+++ components/About/Avatar.js
@@ -1,28 +1,29 @@
 import React from "react";
 import { Div, Image } from "@kits";
-import { logoLg } from "@images";
 
 export function AboutAvatar(props) {
   return (
     <Div flex={[, "center"]}>
-      {/* <abbr title=\"World Health Organization\"> */}
       <Div
         width="11rem"
         height="11rem"
         curve="circle"
-        // bg="brand"
         position="relative"
         overflow="hidden"
         mb="3"
       >
-        <Image
-          src="/images/me.jpg"
-          placeholder="blur"
-          blurDataURL="/images/me.jpg"
-          layout="fill"
-          fit="cover"
-          maximizable
-        />
+        {props.static ? (
+          <Div as="img" src="/images/me.jpg" matchParent />
+        ) : (
+          <Image
+            src="/images/me.jpg"
+            placeholder="blur"
+            blurDataURL="/images/me.jpg"
+            layout="fill"
+            fit="cover"
+            maximizable
+          />
+        )}
       </Div>
     </Div>
   );
