fa5c55bcb34ab56d19e1bf9f2c5e5d5465d7a5fe
Milad <dev.miladmaps@gmail.com>
2022-02-21 11:09:41 +0330

    download resume

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -3,7 +3,7 @@ import { Div } from "@kits";
 import { HeaderNav } from "./Nav";
 import { HeaderChangeLanguage } from "./LanguagePicker";
 import dynamic from "next/dynamic";
-// import { DownloadResume } from "./DownloadResume";
+import { DownloadResume } from "./DownloadResume";
 
 const HeaderSwitchTheme = dynamic(
   () => import("./SwitchTheme").then((module) => module.HeaderSwitchTheme),
@@ -27,7 +27,7 @@ export function Header(props) {
         </Div>
         <Div width="auto">{/* <Logo /> */}</Div>
         <Div width="35%" flex={[, "end"]}>
-          {/* <DownloadResume /> */}
+          <DownloadResume />
           <HeaderChangeLanguage />
           <HeaderSwitchTheme />
         </Div>
