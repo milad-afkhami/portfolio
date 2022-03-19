53b9d4375ca9b5d5ee30e2875aa3f30f0458a662
Milad <miladmaps@gmail.com>
2022-03-20 01:53:17 +0330

    color util - refactor

--- stylesheets/utils/color.js
+++ stylesheets/utils/color.js
@@ -1,17 +1,14 @@
-const types = [
-  "",
-  "-hover",
-  "-light",
-  "-lighter",
-  "-dark",
-  "-pale",
-  "-paler",
-  "-variant",
-];
+import { Color } from "@models";
+
+// const types = ["", "-hover", "-light", "-lighter", "-dark", "-pale", "-paler", "-variant"];
+const types = Object.keys(new Color()).map((type) =>
+  type === "main" ? "" : `-${type}`
+);
+
 /**
  * creates css variables from different types of a single color
  * @example ...color("NAME", "HOVER", "LIGHT", "LIGHTER", "DARK", "PALE", "VARIANT")
- * */
+ **/
 export function color(name, ...colors) {
   return (colors || []).reduce((acc, cv, i) => {
     if (cv) acc[`--${name + types[i]}`] = cv;
