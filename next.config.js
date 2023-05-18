e6bae5f5b1930889a16a95792b0f12f9249e216f
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-18 22:28:33 +0330

    refactor(env): moving environment variables to .env files

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import("next").NextConfig} */
 module.exports = {
-  env: {
-    // REACT_APP_BASE_URL: "https://mili-portfolio.vercel.app",
-    REACT_APP_BASE_URL: "https://milad-afkhami.com",
-    REACT_APP_IMAGE_BASE_URL: "https://2q23wr5n.tinifycdn.com",
-    REACT_APP_EXTERNAL_API_BASE_URL: "/api",
-  },
   i18n,
   compiler: {
     styledComponents: true,
