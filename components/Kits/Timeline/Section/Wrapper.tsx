6ba2596028ccc3d96aa237f3c5853008c5553c3f
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-18 22:17:28 +0330

    refactor: convert timeline kit wrapper to an styled component

---
+++ components/Kits/Timeline/Section/Wrapper.tsx
@@ -0,0 +1,30 @@
+import { styled } from "goober";
+
+const TimelineSectionWrapper = styled("div")`
+  margin: var(--spacing-5) 0;
+  padding: var(--spacing-4) var(--spacing-3);
+  border-radius: var(--curve-sm);
+  background-color: var(--color-bg-secondary-main);
+  &:last-child::before {
+    content: "";
+    border: 2px solid var(--color-border-light);
+    height: calc(100% + 1rem);
+    position: absolute;
+    left: -2rem;
+    top: 1rem;
+  }
+  ::after {
+    content: "";
+    border: var(--color-assistive-success);
+    border-width: 4px;
+    width: 2rem;
+    height: 2rem;
+    position: absolute;
+    left: -3rem;
+    top: -1rem;
+    border-radius: var(--curve-circle);
+    box-shadow: 0px 0px 4px 1px var(--color-assistive-success);
+  }
+`;
+
+export default TimelineSectionWrapper;
--- components/Kits/Timeline/Section/index.tsx
+++ components/Kits/Timeline/Section/index.tsx
@@ -1,4 +1,5 @@
 import Div from "@kits/Div";
+import TimelineSectionWrapper from "./Wrapper";
 import TimelineSectionYear, { type TimelineSectionYearProps } from "./Year";
 import TimelineSectionItem, { type TimelineSectionItemProps } from "./Item";
 import type { FC } from "react";
@@ -12,43 +13,14 @@ const TimelineSection: FC<TimelineSectionProps> = (props) => {
   const { year, items } = props;
 
   return (
-    <Div
-      className="timeline-section"
-      position="relative"
-      my="5"
-      px="3"
-      py="4"
-      curve="sm"
-      bg="bg-secondary"
-      before={{
-        border: "border-light",
-        borderW: 2,
-        height: "calc(100% + 1rem)",
-        position: "absolute",
-        dimensions: { left: "-2rem", top: "1rem" },
-        css: last ? {} : { content: `\"\"` },
-      }}
-      after={{
-        border: "assistive-success",
-        borderW: 4,
-        width: "2rem",
-        height: "2rem",
-        position: "absolute",
-        dimensions: { left: "-3rem", top: "-1rem" },
-        curve: "circle",
-        css: {
-          content: `\"\"`,
-          boxShadow: "0px 0px 4px 1px var(--color-assistive-success)",
-        },
-      }}
-    >
+    <TimelineSectionWrapper>
       <TimelineSectionYear year={year} />
       <Div ml="3" as="ul">
         {items.map((item, i) => (
           <TimelineSectionItem key={i} index={i} text={item} />
         ))}
       </Div>
-    </Div>
+    </TimelineSectionWrapper>
   );
 };
 
