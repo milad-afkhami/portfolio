1facff8c13aa72fa5adad8b8abd8258a1d490bde
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-18 23:30:26 +0330

    refactor(cards): replacing `Text` kit with `useTranslation` hook

--- components/Cards/Blog/Item/Footer.tsx
+++ components/Cards/Blog/Item/Footer.tsx
@@ -1,12 +1,14 @@
 import Div from "@kits/Div";
 import Text from "@kits/Text";
 import Icon from "@kits/Icon";
+import useTranslation from "@hooks/useTranslation";
 import type { FC } from "react";
 
 type BlogCardFooterProps = Pick<IBlog, "publishedAt" | "readingTime" | "views">;
 
 const BlogCardFooter: FC<BlogCardFooterProps> = (props) => {
   const { publishedAt = "", readingTime } = props;
+  const { t } = useTranslation();
 
   return (
     <Div
@@ -18,20 +20,19 @@ const BlogCardFooter: FC<BlogCardFooterProps> = (props) => {
       borderSide="top"
     >
       <Div>
-        <Text color="text-secondary" size="xsm" tag="time">
+        <Text as="time" color="text-secondary" size="xsm">
           {publishedAt}
         </Text>
       </Div>
       {readingTime ? (
-        <Div flex={["center", "center"]}>
+        <Div flex={["center", "center"]} gap="2">
           <Text
-            mx="2"
             color="text-secondary"
             size="xsm"
-            translationVariables={{ time: readingTime }}
             css={{ letterSpacing: "normal" }}
+            noTranslation
           >
-            blog.meta.timeToReadValue
+            {t("blog.meta.timeToReadValue", { time: readingTime })}
           </Text>
           {/* <Text mx="2" color="text-secondary" size="xsm" translationVariables={{ time: readingTime }}>blog.meta.timeToReadValue</Text> */}
           <Icon name="clock" color="text-secondary" size="sm" />
