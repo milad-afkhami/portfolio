1eb9e7d1c17efe09fd9a7d8e295fe7062ebe95d3
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-18 22:25:29 +0330

    refactor(layout): moving Layout content to index file

---
+++ components/Layout/index.tsx
@@ -0,0 +1,26 @@
+import Div from "@kits/Div";
+import Container from "@kits/Container";
+import Header from "./Header";
+import dynamic from "next/dynamic";
+import { APP_WRAPPER_ELEMENT_ID } from "@constants/dom";
+import type { HOCFunctionalComponent, LayoutConfig } from "@_types/components";
+
+// const GithubCorner = dynamic(() => import("./GithubCorner").then((module) => module.GithubCorner), { ssr: false });
+const Footer = dynamic(() => import("./Footer"), { ssr: false });
+
+const Layout: HOCFunctionalComponent<LayoutConfig> = function (props = {}) {
+  const { children, noHeader, noFooter } = props || {};
+
+  return (
+    <Div position="relative" id={APP_WRAPPER_ELEMENT_ID} mh="100vh">
+      <Container>
+        {/* <GithubCorner /> */}
+        {!noHeader && <Header />}
+        <Div>{children}</Div>
+        {!noFooter && <Footer />}
+      </Container>
+    </Div>
+  );
+};
+
+export default Layout;
