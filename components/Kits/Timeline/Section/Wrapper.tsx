2b72fe178f15637170885a4f3576f6619924c5d4
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-21 00:58:10 +0330

    fix(kits): timeline section wrapper stylesheets

--- components/Kits/Timeline/Section/Wrapper.tsx
+++ components/Kits/Timeline/Section/Wrapper.tsx
@@ -1,11 +1,12 @@
 import { styled } from "goober";
 
 const TimelineSectionWrapper = styled("div")`
-  margin: var(--spacing-5) 0;
-  padding: var(--spacing-4) var(--spacing-3);
+  margin: var(--spacing-7) 0;
+  padding: var(--spacing-5) var(--spacing-4);
   border-radius: var(--curve-sm);
   background-color: var(--color-bg-secondary-main);
-  &:last-child::before {
+  position: relative;
+  &:not(:last-child)::before {
     content: "";
     border: 2px solid var(--color-border-light-main);
     height: calc(100% + 1rem);
@@ -13,10 +14,11 @@ const TimelineSectionWrapper = styled("div")`
     left: -2rem;
     top: 1rem;
   }
-  ::after {
+  &::after {
     content: "";
-    border: var(--color-assistive-success-main);
+    border-color: var(--color-assistive-success-main);
     border-width: 4px;
+    border-style: solid;
     width: 2rem;
     height: 2rem;
     position: absolute;
