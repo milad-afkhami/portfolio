bdbe23e6a017ca1a82dbb90068c95ef78fd7ef7a
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-20 12:31:18 +0330

    fix: convert languages config into a cjs module

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,15 @@
 // const path = require('path')
 // const localePath = path.resolve('./public/locales')
 
+const { languages, defaultLanguage } = require("./configs/languages");
+
 /** @type {import("next-i18next").UserConfig} */
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
-  debug: process.env.NODE_ENV === "development",
+  // debug: process.env.NODE_ENV === "development",
   i18n: {
-    defaultLocale: "fa",
-    locales: ["fa"],
+    defaultLocale: defaultLanguage,
+    locales: Object.keys(languages),
   },
   fallbackNS: "common",
   // localePath,
