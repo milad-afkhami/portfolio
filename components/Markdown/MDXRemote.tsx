1759b1e67e173a141be38cf3a754618597fb5827
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-21 01:01:29 +0330

    fix: markdown mdx remote static typing

--- components/Markdown/MDXRemote.tsx
+++ components/Markdown/MDXRemote.tsx
@@ -1,14 +1,20 @@
 import Div from "@kits/Div";
 import Image from "@kits/Image";
 import { MDXRemote as NextMDXRemote } from "next-mdx-remote";
+import type { ReactNode } from "react";
 
-const components = { Image, Div };
+const defaultComponents: Parameters<typeof NextMDXRemote>[0]["components"] = {
+  Div: Div as unknown as ReactNode,
+  Image: Image as unknown as ReactNode,
+};
 
-const MDXRemote = function (props) {
+const MDXRemote = function (props: Parameters<typeof NextMDXRemote>[0]) {
+  const { components } = props;
+
   return (
     <NextMDXRemote
       {...props}
-      components={{ ...components, ...(props.components || {}) }}
+      components={{ ...defaultComponents, ...(components || {}) }}
     />
   );
 };
