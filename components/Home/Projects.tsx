8ac7bede772c7bb809a80b3ce4c5d8d69b869ede
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-20 12:37:32 +0330

    refactor: home project components folder structure

---
+++ components/Home/Projects.tsx
@@ -0,0 +1,11 @@
+import Div from "@kits/Div";
+import ProjectCards from "@components/Cards/Project";
+import type { FC } from "react";
+
+const HomeProjects: FC = () => (
+  <Div my="4">
+    <ProjectCards />
+  </Div>
+);
+
+export default HomeProjects;
