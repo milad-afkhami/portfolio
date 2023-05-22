0126072c0ddadbae893951d34fa02c897c3807a7
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-23 01:12:08 +0330

    feat(layout): change download resume button to normal button kit

--- components/Layout/Header/DownloadResume.tsx
+++ components/Layout/Header/DownloadResume.tsx
@@ -1,7 +1,4 @@
-import Div from "@kits/Div";
-import Icon from "@kits/Icon";
-import Text from "@kits/Text";
-import { responsiveUpperBoundBreakpoint } from "@configs/general";
+import Button from "@kits/Button";
 import type { FC } from "react";
 
 const DownloadResume: FC = () => {
@@ -9,38 +6,15 @@ const DownloadResume: FC = () => {
     window.open("/resume_milad-afkhami.pdf", "__blank");
 
   return (
-    <Div
-      curve="xsm"
-      responsive={{ [responsiveUpperBoundBreakpoint]: { width: "8rem" } }}
-      flex={["center"]}
-      height="2.5rem"
-      width="8rem"
-      mw="7rem"
-      cursor="pointer"
-      bg="assistive-info-main"
-      hoverBg="assistive-info-light"
-      px="3"
-      mx="1"
-      // onClick={printResume}
+    <Button
+      variant="contained"
+      text="Download Resume"
       onClick={handleDownloadResume}
-      css={{ gap: "var(--spacing-2)" }}
-    >
-      <Div flex={["center", "center"]}>
-        <Icon
-          color="bg-primary-main"
-          hoverColor="bg-primary-main"
-          size="lg"
-          bold
-          name="download"
-        />
-      </Div>
-      <Div>
-        <Text ns="layout" color="bg-primary-main" size="sm" bold>
-          header.downloadResume
-        </Text>
-      </Div>
-    </Div>
+      icon="download"
+    />
   );
+
+  // return <Div curve="xsm" responsive={{ [responsiveUpperBoundBreakpoint]: { width: "8rem" } }} flex={["center"]} height="2.5rem" width="8rem" mw="7rem" cursor="pointer" bg="assistive-info-main" hoverBg="assistive-info-light" px="3" mx="1" onClick={handleDownloadResume} css={{ gap: "var(--spacing-2)" }}><Div flex={["center", "center"]}><Icon color="bg-primary-main" hoverColor="bg-primary-main" size="lg" bold name="download" /></Div><Div><Text ns="layout" color="bg-primary-main" size="sm" bold>header.downloadResume</Text></Div></Div>
 };
 
 export default DownloadResume;
