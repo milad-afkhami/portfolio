cb8ebb349446519c73a350aa78c6e442e8a8a3f0
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-25 15:43:04 +0330

    fix: improve static typing of the `useEventListener` hook

--- hooks/useEventListener.ts
+++ hooks/useEventListener.ts
@@ -1,20 +1,34 @@
 import { useEffect, useRef } from "react";
 
-const useEventListener = (eventName, handler, element = window) => {
-  const handlerRef = useRef();
+interface EventListenerHook {
+  (
+    eventName: string,
+    listener: EventListener,
+    element?: ReturnType<typeof document.querySelector> | Window
+  ): void;
+}
+
+const useEventListener: EventListenerHook = (
+  eventName,
+  listener,
+  element = window
+) => {
+  const handlerRef = useRef<EventListener>();
 
   useEffect(() => {
-    handlerRef.current = handler;
-  }, [handler]);
+    handlerRef.current = listener;
+  }, [listener]);
 
   useEffect(() => {
     const isSupported = element && element.addEventListener;
+
     if (!isSupported) return;
 
-    const eventListener = (event) => handlerRef.current(event);
+    const eventListener = (event: Event) => handlerRef.current?.(event);
 
     element.addEventListener(eventName, eventListener);
 
+    // eslint-disable-next-line consistent-return
     return () => element.removeEventListener(eventName, eventListener);
   }, [eventName, element]);
 };
