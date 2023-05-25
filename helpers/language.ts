0295c9dd5f6a25d7c28284036b84cca2e4fd9165
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-25 17:23:00 +0330

    fix(helpers): improve static typing of language

--- helpers/language.ts
+++ helpers/language.ts
@@ -1,20 +1,20 @@
-import PageAttr from "@helpers/pageAttribute";
+// import PageAttr from "@helpers/pageAttribute";
 import Cookies from "@utils/cookies";
 import { languages, defaultLanguage } from "@configs/languages";
 import { LOCALE_COOKIE_KEY } from "@constants/cookies";
 
 export default class LanguageHelper {
-  static initialLanguageSetup(locale) {
-    // const _locale: Languages =
-    //   locale || Cookies.get(LOCALE_COOKIE_KEY, null) || defaultLanguage;
-    // const language = languages[_locale];
-    // PageAttr.setDirection(language.direction);
-    // // PageAttr.setLanguage(language.name); handled by next.js now
-    // PageAttr.setFont(language.name);
-    // PageAttr.setPrefix();
-    // // if (i18n.language !== language.name)
-    // i18n.changeLanguage(language.name);
-  }
+  // static initialLanguageSetup(locale) {
+  // const _locale: Languages =
+  //   locale || Cookies.get(LOCALE_COOKIE_KEY, null) || defaultLanguage;
+  // const language = languages[_locale];
+  // PageAttr.setDirection(language.direction);
+  // // PageAttr.setLanguage(language.name); handled by next.js now
+  // PageAttr.setFont(language.name);
+  // PageAttr.setPrefix();
+  // // if (i18n.language !== language.name)
+  // i18n.changeLanguage(language.name);
+  // }
 
   static changeLanguage(locale: Languages) {
     const language = languages[locale];
