bc09d0207b68325f2c5d9253e33cc38e4360fcf3
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-05-29 17:55:08 +0330

    fix(i18n): temporarily adding `en` as the only locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,16 +1,14 @@
 // const path = require('path')
 // const localePath = path.resolve('./public/locales')
 
-const { languages, defaultLanguage } = require("./configs/languages");
+// const { languages, defaultLanguage } = require("./configs/languages");
 
 /** @type {import("next-i18next").UserConfig} */
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   // debug: process.env.NODE_ENV === "development",
-  i18n: {
-    defaultLocale: defaultLanguage,
-    locales: Object.keys(languages),
-  },
+  i18n: { defaultLocale: "en", locales: ["en"] },
+  // i18n: { defaultLocale: defaultLanguage, locales: Object.keys(languages) },
   fallbackNS: "common",
   // localePath,
   reloadOnPrerender: process.env.NODE_ENV === "development",
