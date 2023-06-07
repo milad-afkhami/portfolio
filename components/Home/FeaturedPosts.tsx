820452374b456fa084356ae8590ac8e907a637b0
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-06-07 13:13:18 +0330

    fix: changing the static blogs in home page

--- components/Home/FeaturedPosts.tsx
+++ components/Home/FeaturedPosts.tsx
@@ -4,9 +4,10 @@ import { useTranslation } from "next-i18next";
 import type { FC } from "react";
 
 const homeBlogs = [
-  "data-fetching-pattern-preference",
-  "rethink-using-redux",
+  // "data-fetching-pattern-preference",
+  "one-on-one",
   "front-end-learning-roadmap",
+  "rethink-using-redux",
   // "redux-clean-code",
   // "homophobia",
 
@@ -26,7 +27,10 @@ const FeaturedPosts: FC<FeaturedPostsProps> = (props) => {
 
   // const isMediumScreen = useMediaQuery({ minWidth: breakpoints.sm, maxWidth: breakpoints.md });
 
-  const items = posts.filter(({ slug }) => homeBlogs.includes(slug));
+  // const items = posts.filter(({ slug }) => homeBlogs.includes(slug));
+  const items = homeBlogs.map((slug) =>
+    posts.find((post) => post.slug === slug)
+  ) as Array<IBlog>;
 
   return (
     <Div my="4">
