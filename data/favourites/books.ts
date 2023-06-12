55cdfaf840c2ee7091acb85452c12f3543cbd088
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-06-12 20:05:19 +0330

    feat(data): adding new books to favourites

--- data/favourites/books.ts
+++ data/favourites/books.ts
@@ -1,14 +1,16 @@
 const favouriteBooksData = [
   "the-making-of-a-manager",
   "nvc",
-  "republic",
+  // "republic",
+  "no-rules-rules",
   "the-power-of-habit",
   "the-unbearable-lightness-of-being",
+  "the-joy-of-missing-out",
   "masnavi",
   // "quran",
   "golestan",
   "nahj-al-balagha",
-  "tafsir-nemooneh",
+  // "tafsir-nemooneh",
 ];
 
 export default favouriteBooksData;
