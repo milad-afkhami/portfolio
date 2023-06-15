063b03d47e49a767f557c5e038387652d9ee8f59
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-06-16 00:17:49 +0330

    refactor(hooks): convert type to interface in useToggle hook

--- hooks/useToggle.ts
+++ hooks/useToggle.ts
@@ -1,6 +1,8 @@
 import { useState, useCallback, useDebugValue } from "react";
 
-type ToggleHook = (initialState?: boolean) => [boolean, () => void];
+interface ToggleHook {
+  (initialState?: boolean): [boolean, () => void];
+}
 
 /** This hook Takes a parameter with value true or false and toggles that value to opposite. It's useful when we want to take some action into it's opposite action, for example: show and hide modal, show more/show less text, open/close side menu. */
 const useToggle: ToggleHook = (initialState = false) => {
