aa9d9108aa15d928e179c11723488fe5dd237bdc
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-11 23:54:51 +0330

    feat: adding how-to-thrive-in-the-virtual-workplace book to favourites

--- data/favourites/books.ts
+++ data/favourites/books.ts
@@ -1,16 +1,14 @@
 const favouriteBooksData = [
   "the-making-of-a-manager",
   "nvc",
-  // "republic",
+  "how-to-thrive-in-the-virtual-workplace",
   "no-rules-rules",
   "the-power-of-habit",
   "the-unbearable-lightness-of-being",
   "the-joy-of-missing-out",
   "masnavi",
-  // "quran",
   "golestan",
   "nahj-al-balagha",
-  // "tafsir-nemooneh",
 ];
 
 export default favouriteBooksData;
