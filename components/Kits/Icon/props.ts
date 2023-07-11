b0c95a25e30ba55174032ae829695731b5afec4e
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-11 23:02:07 +0330

    feat(icons): font icons version 1.3

--- components/Kits/Icon/props.ts
+++ components/Kits/Icon/props.ts
@@ -3,7 +3,7 @@ import type { CSSAttribute } from "goober";
 
 // prettier-ignore
 // export type IconName = "github" | "code" | "moon" | "hash" | "nested-list" | "clock" | "time" | "history" | "list" | "info" | "headphones" | "music" | "song" | "film" | "show" | "book" | "suitcase" | "sun" | "download" | "copy-outline" | "focus-center" | "timeline" | "calendar" | "gallery" | "tools" | "measure" | "education" | "chevron-right" | "whatsapp" | "telegram" | "skype" | "linkedin" | "email";
-export type IconName =  "close" | "warning-outlined" | "check" | "info-outlined" | "github" | "code" | "moon" | "hash" | "nested-list" | "clock" | "time" | "history" | "list" | "info" | "headphones" | "music" | "song" | "film" | "show" | "book" | "suitcase" | "sun" | "warning" | "check-outlined" | "education" | "download" | "copy-outline" | "focus-center" | "timeline" | "calendar" | "gallery" | "tools" | "measure" | "chevron-right" | "whatsapp" | "telegram" | "skype" | "linkedin" | "email"
+export type IconName =  "external-link" | "close" | "warning-outlined" | "check" | "info-outlined" | "github" | "code" | "moon" | "hash" | "nested-list" | "clock" | "time" | "history" | "list" | "info" | "headphones" | "music" | "song" | "film" | "show" | "book" | "suitcase" | "sun" | "warning" | "check-outlined" | "education" | "download" | "copy-outline" | "focus-center" | "timeline" | "calendar" | "gallery" | "tools" | "measure" | "chevron-right" | "whatsapp" | "telegram" | "skype" | "linkedin" | "email"
 
 export default interface IconProps extends HTMLAttributes<Element> {
   name: IconName;
