9861c6ee11e339c738752cb85b81dd617a505891
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-12 13:29:57 +0330

    refactor: improve environment variables format and remove unused variables

--- types/env.d.ts
+++ types/env.d.ts
@@ -4,6 +4,6 @@ namespace NodeJS {
     PORT: string;
     NEXT_PUBLIC_API_BASE_URL: string;
     NEXT_PUBLIC_BASE_URL: string;
-    NEXT_PUBLIC_IMAGE_BASE_URL: string;
+    NEXT_PUBLIC_IMAGE_BASE_URL?: string;
   }
 }
