69b437fca56bd8e32a7dd4e7529801b3b107bcd1
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-16 20:41:23 +0330

    refactor: migrating to style-wiz
    installing & replacing import statements

--- components/Kits/Div/index.ts
+++ components/Kits/Div/index.ts
@@ -1,36 +1,4 @@
-import { styled } from "goober";
-import { forwardRef } from "react";
-import __reduce from "lodash-es/reduce";
-import convertPropsToStyles from "./convertPropsToStyles";
-import responsiveProps from "@stylesheets/utils/responsiveProps";
-import type DivProps from "./props";
-
-/**
- * renders a custom div component with needed stylesheets.
- * Read more about it in Documentation( {@link https://milad-afkhami.com/gists/div} )
- */
-
-const Div = styled(
-  "div",
-  forwardRef
-)<DivProps>((props) => {
-  const { responsive, after, before, hover, ...restProps } = props;
-  return {
-    ...convertPropsToStyles(restProps as DivProps),
-    ...(responsive
-      ? __reduce(
-          responsiveProps(responsive),
-          (res: Dictionary<unknown>, v, k) => {
-            res[k] = convertPropsToStyles(v);
-            return res;
-          },
-          {}
-        )
-      : {}),
-    ...(after ? { "&:after": convertPropsToStyles(after) } : {}),
-    ...(before ? { "&:before": convertPropsToStyles(before) } : {}),
-    ...(hover ? { "&:hover": convertPropsToStyles(hover) } : {}),
-  };
-});
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Div } from "style-wiz";
 
 export default Div;
