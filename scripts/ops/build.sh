a7f35f4aaf794f03acc02d7f3470c9c31309b315
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-23 20:27:54 +0330

    refactor: change the path of next build command

--- scripts/ops/build.sh
+++ scripts/ops/build.sh
@@ -7,7 +7,7 @@ echo -e "${BLUE}Removing next cache started${NO_COLOR}\n" &&
     yarn stylesheets &&
     echo -e "\n${GREEN}Generating build time stylesheets succeeded${NO_COLOR}\n" &&
     echo -e "${BLUE}Generating production build started${NO_COLOR}\n" &&
-    GENERATE_SOURCEMAP=false node_modules/next/dist/bin/next build &&
+    GENERATE_SOURCEMAP=false node_modules/.bin/next build &&
     echo -e "${BLUE}Generating production build succeeded${NO_COLOR}\n"
 
 exit $?
