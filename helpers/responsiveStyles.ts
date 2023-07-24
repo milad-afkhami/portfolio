639ea6a8095a3373b40d5235d6a79b8f696dc40b
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-24 17:37:00 +0330

    docs: adding jsDoc to responsiveStyles helper

--- helpers/responsiveStyles.ts
+++ helpers/responsiveStyles.ts
@@ -1,9 +1,22 @@
 import breakpoints from "@constants/stylesheets/breakpoints.json";
 import type { CSSAttribute } from "goober";
 
-const responsiveStyles = (breakpoint: Breakpoints, styles: CSSAttribute) =>
-  styles
-    ? { [`@media (min-width: ${breakpoints[breakpoint]}px)`]: styles }
-    : {};
+/**
+ * This method helps us in generating responsive stylesheet objects
+ * @param breakpoint the lower bound breakpoint,
+ * the stylesheets will apply to layouts with a width bigger than this breakpoint value(mobile first)
+ * @param styles css stylesheets object
+ * @example
+ * ```tsx
+ * <Div styles={responsiveStyles("md", { display: "flex" })} />
+ * ```
+ */
+const responsiveStyles = (breakpoint: Breakpoints, styles: CSSAttribute) => {
+  if (styles) {
+    return { [`@media (min-width: ${breakpoints[breakpoint]}px)`]: styles };
+  }
+
+  return {};
+};
 
 export default responsiveStyles;
