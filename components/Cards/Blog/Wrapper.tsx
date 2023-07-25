c1fd494bbede17c2b86c21c59b60c9573cd4f0e5
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-25 12:58:52 +0330

    revert(cards): responsive grid in blog cards

--- components/Cards/Blog/Wrapper.tsx
+++ components/Cards/Blog/Wrapper.tsx
@@ -1,7 +1,6 @@
 // #region imports
 import { Div } from "style-wiz";
 import responsiveStyles from "@helpers/responsiveStyles";
-import { responsiveUpperBoundBreakpoint } from "@configs/general";
 import type { HOCFunctionalComponent } from "@_types/components";
 // #endregion
 
@@ -11,9 +10,10 @@ const BlogCardsWrapper: HOCFunctionalComponent = (props) => {
   return (
     <Div
       grid={["repeat(1, 1fr)", , "1rem", "1rem", "row"]}
-      styles={responsiveStyles(responsiveUpperBoundBreakpoint, {
-        gridTemplateColumns: "repeat(3, 1fr)",
-      })}
+      styles={{
+        ...responsiveStyles("md", { gridTemplateColumns: "repeat(2, 1fr)" }),
+        ...responsiveStyles("lg", { gridTemplateColumns: "repeat(3, 1fr)" }),
+      }}
       overflowX="auto"
       thinScrollbar
       pb="2"
