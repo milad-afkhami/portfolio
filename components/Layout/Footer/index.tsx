0027df1cf7e116bb46af270eb0d2938453f70a3a
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-08-15 21:03:48 +0330

    refactor(layout): remove unused grid kit from footer

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,17 +1,11 @@
 import { Div } from "style-wiz";
-import { Row, Col } from "@kits/Grid";
 import FooterLinks from "./Links";
 import type { FC } from "react";
 // import FooterSocials from "./Socials";
 
 const Footer: FC = () => (
   <Div mt="5" py="5" mw="100%" border="border-light-main" borderSide="top">
-    <Row>
-      <Col xs="12" lg="8">
-        <FooterLinks />
-      </Col>
-      {/* <Col xs="12" lg="4"><FooterSocials /></Col> */}
-    </Row>
+    <FooterLinks />
   </Div>
 );
 
