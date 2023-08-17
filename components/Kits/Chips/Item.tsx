c5eec859fc62d9ae8d925bce33d235306ef25dfa
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-08-17 17:42:33 +0330

    feat: add secondary variation to the "Chips" kit

--- components/Kits/Chips/Item.tsx
+++ components/Kits/Chips/Item.tsx
@@ -5,21 +5,35 @@ import type { FC } from "react";
 export interface ChipProps {
   label: string;
   link?: string;
+  secondary?: boolean;
 }
 
 const Chip: FC<ChipProps> = (props) => {
-  const { label, link } = props;
+  const { label, link, secondary } = props;
+
+  const paletteAndVariation = secondary ? "bg-tertiary" : "brand-primary";
 
   return (
     <Div
-      {...(link ? { as: "a", href: link, target: "_blank" } : {})}
-      bg="brand-primary-main"
-      hoverBg="brand-primary-hover"
+      {...(link
+        ? {
+            as: "a",
+            href: link,
+            target: "_blank",
+            hoverBg: `${paletteAndVariation}-hover`,
+          }
+        : {})}
+      bg={`${paletteAndVariation}-main`}
       px="2"
       curve="xsm"
       display="flex"
     >
-      <Text size="md" bold color="brand-primary-contrast" noTranslation>
+      <Text
+        size="md"
+        bold
+        color={`${paletteAndVariation}-contrast`}
+        noTranslation
+      >
         {label}
       </Text>
     </Div>
--- components/Kits/Chips/index.tsx
+++ components/Kits/Chips/index.tsx
@@ -4,15 +4,16 @@ import type { FC } from "react";
 
 interface ChipsProps {
   items: ChipProps[];
+  secondary?: boolean;
 }
 
 const Chips: FC<ChipsProps> = (props) => {
-  const { items } = props;
+  const { items, secondary } = props;
 
   return (
     <Div flex={["center", "flex-start", , "wrap"]} gap="2">
       {items.map((item) => (
-        <Chip key={item.label} {...item} />
+        <Chip key={item.label} {...item} secondary={secondary} />
       ))}
     </Div>
   );
