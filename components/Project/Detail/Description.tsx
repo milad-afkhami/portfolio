4d1e38db80d26e848bda32f93dbd9add71fc6539
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-08-20 22:23:42 +0330

    feat(cards): add section title to project detail description

--- components/Project/Detail/Description.tsx
+++ components/Project/Detail/Description.tsx
@@ -1,5 +1,6 @@
 import { Div } from "style-wiz";
 import Text from "@kits/Text";
+import SectionTitle from "@components/Layout/Title/Section";
 import type { FC } from "react";
 
 const ProjectDetailDescription: FC<Pick<IProject, "slug">> = (props) => {
@@ -7,6 +8,7 @@ const ProjectDetailDescription: FC<Pick<IProject, "slug">> = (props) => {
 
   return (
     <Div mb="3">
+      <SectionTitle icon="document" title="description" ns="projects" />
       <Text size="lg" ns="projects">{`${slug}.description`}</Text>
       {/* /{" "}<Text size="md" bold as="time" styles={{ textDecoration: "underline" }}>{`${props.date}`}</Text> */}
     </Div>
