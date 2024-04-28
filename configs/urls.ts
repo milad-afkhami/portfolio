8173d9db4ac9a054e7d14cf174f5d30ac6735def
Milad Afkhami <57092046+milad-afkhami@users.noreply.github.com>
2024-04-28 10:13:37 +0330

    feat(configs): update resume drive link

--- configs/urls.ts
+++ configs/urls.ts
@@ -2,8 +2,7 @@ const apiBaseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const appBaseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const imageBaseURL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
 
-// `/documents/${encodeURIComponent(`Milad Afkhami's Resume.pdf`)}`
 const resumeURL =
-  "https://drive.google.com/file/d/1ah3eWYiHiZyLX8vDGJpZRtZQP2ZeTjVU/view?usp=drive_link";
+  "https://drive.google.com/file/d/1IyVUcd_NWT0PKnnYI5OAPVI7SDgV4w-3/view";
 
 export { apiBaseURL, appBaseURL, imageBaseURL, resumeURL };
