78ab8548be543d7e1f25ef697818733b3227daaf
Milad Afkhami <57092046+milad-afkhami@users.noreply.github.com>
2024-05-02 00:14:01 +0330

    feat: update resume google drive URL

--- configs/urls.ts
+++ configs/urls.ts
@@ -3,6 +3,6 @@ const appBaseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const imageBaseURL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
 
 const resumeURL =
-  "https://drive.google.com/file/d/1IyVUcd_NWT0PKnnYI5OAPVI7SDgV4w-3/view";
+  "https://drive.google.com/file/d/1JC9rE0VXZUwJxRvc83_gTpZVlP8TkErG/view";
 
 export { apiBaseURL, appBaseURL, imageBaseURL, resumeURL };
