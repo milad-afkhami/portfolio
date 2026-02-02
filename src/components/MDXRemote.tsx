b2469d27a215aeea91a0f4c584a8876acb595cb4
Milad Afkhami <miladmaps@gmail.com>
2026-02-02 19:09:46 +0330

    feat: add sissy agent videos to blog

--- src/components/MDXRemote.tsx
+++ src/components/MDXRemote.tsx
@@ -10,6 +10,18 @@ type Props = {
   components?: ComponentProps<typeof MDXRemoteComponent>["components"];
 };
 
+type VideoProps = {
+  src: string;
+};
+
+function Video({ src }: VideoProps): JSX.Element {
+  return (
+    <div className="relative mx-auto w-[min(1000px,100%)]">
+      <video src={src} autoPlay loop muted playsInline className="mx-auto my-0 w-full overflow-hidden rounded-md" />
+    </div>
+  );
+}
+
 export default function MDXRemote({ content }: Props): JSX.Element {
   return (
     <div className="prose prose-lg max-w-none [&_table]:block [&_table]:overflow-x-auto">
@@ -17,6 +29,7 @@ export default function MDXRemote({ content }: Props): JSX.Element {
         {...content}
         components={{
           Image,
+          Video,
           img: (props) => (
             <div className="relative mx-auto h-52 w-[min(1000px,100%)] md:h-96">
               {/* eslint-disable-next-line jsx-a11y/alt-text */}
